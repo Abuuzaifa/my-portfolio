8442e5de8184cf3eccaec97af1a491777cf7d857
Abuuzaifa <abuzaifa18@gmail.com>
2025-05-26 16:39:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = "Portfolio-Abu";
 root.render(
   <BrowserRouter>
     <App />
